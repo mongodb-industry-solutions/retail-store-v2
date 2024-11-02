ec7b8478cf3fd6d8cfc3e449937307137e311afc
angie <angie.guemes@mongodb.com>
2024-11-02 15:43:42 -0600

    checkout(ui)

--- app/_components/bopisComp/BopisComp.jsx
+++ app/_components/bopisComp/BopisComp.jsx
@@ -1,22 +1,36 @@
 import { Combobox, ComboboxOption } from '@leafygreen-ui/combobox';
-import Image from "next/image";
 
-import styles from './bopisComp.module.css';
+import Banner from '@leafygreen-ui/banner';
 
-const BopisComp = ({storeLocations}) => {
+const BopisComp = ({storeLocations, setSelectedStoreLocation}) => {
+  
+  const onStoreLocationChange = (value)  => {
+    // value is the index of the storeLocations[] selected
+    const storeSelected = storeLocations[value]
+    setSelectedStoreLocation(storeSelected)
+  }
+
   return (
     <div>
+      <Banner className='mb-3 mt-3'>
+        With the right omnichannel ordering strategy, powered by a flexible database like MongoDB Atlas, retailers can unify these touchpoints to offer customers a personalized, convenient, and efficient shopping experience across all channels.
+      </Banner>
+
       <Combobox
         label="Pick your prefered store location"
         description="Your order will be delivered to this store."
         placeholder="Select a store"
+        onChange={(e) => onStoreLocationChange(e)}
       >
         {
-          storeLocations.map((store, index) => 
-            <ComboboxOption 
-              value={`${store._id}-${index}`} 
-              displayName={store.name}
+          storeLocations.map((store, index) => {
+            const {_id, street_and_number, cp, country, state, city, name} = store;
+            return <ComboboxOption 
+              key={`${_id}-${index}`} 
+              value={`${index}-store`} 
+              displayName={`${name} - ${street_and_number}, ${cp} ${city}. ${country}`}
             />
+          }
           )
         }
       </Combobox>
--- app/checkout/page.js
+++ app/checkout/page.js
@@ -2,7 +2,7 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { H1, H3, Disclaimer, Body } from '@leafygreen-ui/typography';
+import { H1, H3, Body } from '@leafygreen-ui/typography';
 import Icon from '@leafygreen-ui/icon';
 import { RadioBox, RadioBoxGroup } from '@leafygreen-ui/radio-box-group';
 
@@ -16,6 +16,8 @@ import styles from './checkout.module.css'
 import Card from '@leafygreen-ui/card';
 import HomeAddressComp from '../_components/homeAddressComp/homeAddressComp';
 import BopisComp from '../_components/bopisComp/BopisComp';
+import ProductsModalComp from '../_components/productsModalComp/ProductsModalComp';
+import { CardSkeleton } from '@leafygreen-ui/skeleton-loader';
 
 const shippingMethods = [
     {value: 'home', label: 'Send to my home address'},
@@ -28,13 +30,16 @@ export default function Page() {
     const selectedUser = useSelector(state => state.User.selectedUser);
     const [shippingMethod, setShippingMethod] = useState(shippingMethods[0].value)
     const [storeLocations, setStoreLocations] = useState([])
-
+    const [selectedStoreLocation, setSelectedStoreLocation] = useState(null)
+    const [productDetailsOpened, setProductDetailsOpened] = useState(false)
+    
     const onConfirmOrder = () => {
-
+        // TODO: create order backend part
     }
+
     const onShippingMethodChange = (e) => {
-        console.log('onShippingMethodChange', e.target.value)
         setShippingMethod(e.target.value)
+        setSelectedStoreLocation(null)
     }
 
     useEffect(() => {
@@ -59,6 +64,7 @@ export default function Page() {
         const getStoreLocations = async () => {
             try {
                 const result = await fetchStoreLocations();
+                console.log('result', result)
                 if (result !== null) {
                     setStoreLocations(result)
                 }
@@ -82,63 +88,77 @@ export default function Page() {
                 </div>
                 {
                     cart.loading
-                        ? 'Loading...'
+                        ? <div className='mt-3'>
+                            <H3 className="mb-2">Payment details</H3>
+                            <CardSkeleton />
+                            <H3 className="mb-2 mt-3">Products</H3>
+                            <CardSkeleton />
+                            <H3 className="mb-2 mt-3">Shipping address</H3>
+                            <CardSkeleton />
+                        </div>
                         : cart.products?.length < 1
-                            ? <div>
-                                Fill cart randomly
-                            </div>
-                            : <div className='mt-3'>
-                                <H3 className="mb-2">Payment details</H3>
-                                <Card className={styles.cardInfo}>
-                                    <Body><strong>Order: </strong>${cart.totalPrice}</Body>
-                                    <Body><strong>Shipping: </strong>$0</Body>
-                                    <Body><strong>Total: </strong>${cart.totalPrice}</Body>
-                                </Card>
+                        ? <div>
+                            Fill cart randomly 
+                            {/* TODO backend part to add random items to cart */}
+                        </div>
+                        : <div className='mt-3'>
+                            <H3 className="mb-2">Payment details</H3>
+                            <Card className={styles.cardInfo}>
+                                <Body><strong>Order: </strong>${cart.totalPrice}</Body>
+                                <Body><strong>Shipping: </strong>$0</Body>
+                                <Body><strong>Total: </strong>${cart.totalPrice}</Body>
+                            </Card>
 
-                                <H3 className="mb-2 mt-3">Products</H3>
-                                <Card className={styles.cardInfo}>
-                                    <Body><strong>Amount: </strong>{cart.totalAmount} items in cart <Icon className="cursorPointer d-none" glyph="Visibility" /></Body>
-                                </Card>
+                            <H3 className="mb-2 mt-3">Products</H3>
+                            <Card className={styles.cardInfo}>
+                                <Body><strong>Amount: </strong>{cart.totalAmount} items in cart <Icon onClick={() => setProductDetailsOpened(true)} className="cursorPointer" glyph="Visibility" /></Body>
+                            </Card>
 
-                                <H3 className="mb-2 mt-3">Shipping address</H3>
-                                <Card className={styles.cardInfo}>
-                                    <RadioBoxGroup 
-                                        onChange={(e) => onShippingMethodChange(e)} 
-                                        className="radio-box-group-style mb-3"
-                                    >
-                                        {
-                                            shippingMethods.map((method, index) => (
-                                                <RadioBox 
-                                                    checked={index == 0} 
-                                                    value={method.value}
-                                                >
-                                                   {method.label}
-                                                </RadioBox>
-
-                                            ))
-                                        }
-                                    </RadioBoxGroup>
+                            <H3 className="mb-2 mt-3">Shipping address</H3>
+                            <Card className={styles.cardInfo}>
+                                <RadioBoxGroup 
+                                    onChange={(e) => onShippingMethodChange(e)} 
+                                    className="radio-box-group-style mb-3"
+                                >
                                     {
-                                        shippingMethod === shippingMethods[0].value // home
-                                        ? <HomeAddressComp address={selectedUser.address} containerStyle={styles.cardInfo}/>
-                                        :  shippingMethod === shippingMethods[1].value // bopis
-                                        ? <BopisComp containerStyle={styles.cardInfo}/>
-                                        : 'Unrecognized shipping method, please select another option'
-                                    }
-                                </Card>
+                                        shippingMethods.map((method, index) => (
+                                            <RadioBox 
+                                                checked={index == 0} 
+                                                value={method.value}
+                                            >
+                                                {method.label}
+                                            </RadioBox>
 
-                                <div className='d-flex flex-row-reverse mt-3'>
-                                    <Button
-                                        variant='primary'
-                                        onClick={() => onConfirmOrder()}
-                                    >
-                                        Confirm & order
-                                    </Button>
-                                </div>
+                                        ))
+                                    }
+                                </RadioBoxGroup>
+                                {
+                                    shippingMethod === shippingMethods[0].value // home
+                                    ? <HomeAddressComp address={selectedUser.address} containerStyle={styles.cardInfo}/>
+                                    :  shippingMethod === shippingMethods[1].value // bopis
+                                    ? <BopisComp containerStyle={styles.cardInfo} storeLocations={storeLocations} setSelectedStoreLocation={setSelectedStoreLocation}/>
+                                    : 'Unrecognized shipping method, please select another option'
+                                }
+                            </Card>
+
+                            <div className='d-flex flex-row-reverse mt-3'>
+                                <Button
+                                    variant='primary'
+                                    disabled={cart.products?.length === 0 || (shippingMethod === 'bopis' && selectedStoreLocation === null )}
+                                    onClick={() => onConfirmOrder()}
+                                >
+                                    Confirm & order
+                                </Button>
                             </div>
+                        </div>
                 }
             </Container>
             <Footer></Footer>
+            <ProductsModalComp  
+                open={productDetailsOpened} 
+                handleClose={() => setProductDetailsOpened(false)}
+                products={cart.products}
+            />
         </>
     );
 }
