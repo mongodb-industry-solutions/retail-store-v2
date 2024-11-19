a9e9459c3d178cff3355c2cf322ae9cccbf89663
angie <angie.guemes@mongodb.com>
2024-11-19 09:35:52 -0600

    changeStream(api): match pipeline

--- redux/slices/UserSlice.js
+++ redux/slices/UserSlice.js
@@ -65,7 +65,7 @@ const UserSlice = createSlice({
                     break;
                 }
             }
-            console.log('newList', newList)
+            //console.log('newList', newList)
             return {
                 ...state, 
                 orders: {
