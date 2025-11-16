1ca46509fc1ca4389d82d6c07e7d4181c912ee50
Angelica Guemes <97576442+AngelicaGE@users.noreply.github.com>
2025-11-15 22:26:34 -0600

    Split features (#49) (#50)
    * add guide cue logic for digital receipts
    
    * preserve params in About link page
    
    * Feature Split into 3, Added Guided walkthroughs in each feature,
    
    * Feature Split into 3, Added Guided walkthroughs in each feature,
    
    * Added autofill capability
    
    * Feature constant
    
    * Feature constant
    
    * Integrated Guidecue Container component
    
    * Integrated Guidecue Container component and Ready Logic
    
    * fix get user data
    
    * finish feature split changes
    
    ---------
    
    Co-authored-by: Rakshit1798 <rakshitjoshimongodb@gmail.com>

--- app/_components/about/About.jsx
+++ app/_components/about/About.jsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import "../../fonts.css";
@@ -7,81 +6,37 @@ import TeamCard from "../teamcard/TeamCard";
 import Disclaimer from "../disclaimer/Disclaimer";
 
 import Button from "@leafygreen-ui/button";
-import { H1, H3, Body } from '@leafygreen-ui/typography';
+import { H1, H3, Body } from "@leafygreen-ui/typography";
 import { Container } from "react-bootstrap";
 //import Link from "next/link";
 
 const About = () => {
-
-  const githubRepoUrl = "https://github.com/mongodb-industry-solutions/retail-store-v2";
-  const githubRepoMSUrl = "https://github.com/mongodb-industry-solutions/retail-digital-receipts-backend";
+  const githubRepoUrl =
+    "https://github.com/mongodb-industry-solutions/retail-store-v2";
+  const githubRepoMSUrl =
+    "https://github.com/mongodb-industry-solutions/retail-digital-receipts-backend";
 
   return (
-
     <Container>
       <H1>About</H1>
       <div className={styles.firstSection}>
         <div className={styles.disclaimer}>
           <Body className={styles.body}>
-            This ecommerce platform serves as a showcase for the powerful features and capabilities of MongoDB.
-            The products, prices, and any other content on this site are entirely fictional and do not represent a real business. No actual transactions or purchases can be made on this platform.
+            This ecommerce platform serves as a showcase for the powerful
+            features and capabilities of MongoDB. The products, prices, and any
+            other content on this site are entirely fictional and do not
+            represent a real business. No actual transactions or purchases can
+            be made on this platform.
             <br></br>
             <br></br>
-            Thank you for joining us in this demo experience, and we hope you enjoy exploring the capabilities of MongoDB!
+            Thank you for joining us in this demo experience, and we hope you
+            enjoy exploring the capabilities of MongoDB!
           </Body>
-          <Disclaimer/>
+          <Disclaimer />
         </div>
         <div>
           <img className={styles.aboutImg} src="/aboutillo.png"></img>
         </div>
-      </div>
-      <H3>Team</H3>
-      <div className={styles.teamcards}>
-        <TeamCard
-          photo="/rsc/images/team/gen.png"
-          name="Genevieve Broadhead"
-          title="Global Lead, Retail Solutions"
-          subtitle="Product Owner"
-        />
-        <TeamCard
-          photo="/rsc/images/team/prashant.png"
-          name="Prashant Juttukonda"
-          title="Retail Principal, Industry Solutions"
-          subtitle="Product Owner"
-        />
-        <TeamCard
-          photo="/rsc/images/team/rodri.png"
-          name="Rodrigo Leal"
-          title="Retail Principal, Industry Solutions"
-          subtitle="Product Owner"
-        />
-        <TeamCard
-          photo="/rsc/images/team/angie.jpeg"
-          name="Angie Güemes"
-          title="Sr. Specialist, Industry Solutions"
-          subtitle="Full Stack Developer"
-        />
-
-        <TeamCard
-          photo="/rsc/images/team/flor.png"
-          name="Flor Arín"
-          title="Sr. Specialist, Industry Solutions"
-          subtitle="Full Stack Developer"
-        />
-
-        <TeamCard
-          photo="/rsc/images/team/pedro.png"
-          name="Pedro Bereilh"
-          title="Sr. Specialist, Industry Solutions"
-          subtitle="Cloud Engineer"
-        />
-        <TeamCard
-          photo="/rsc/images/team/ainhoa.png"
-          name="Ainhoa Mugica"
-          title="Consultant, Industry Solutions"
-          subtitle="Project Manager"
-        />
-        {/* Add more TeamMemberCard components as needed */}
       </div>
       <H3>Related Resources</H3>
       <div className={styles.githubButton}>
@@ -97,6 +52,64 @@ const About = () => {
             Github: Invoice & Recommendation MS
           </Button>
         </a>
+      </div>
+      <H3>Team</H3>
+      <div className={styles.teamcards}>
+        <TeamCard
+          photo="/rsc/images/team/gen.png"
+          name="Genevieve Broadhead"
+          title="Global Lead, Retail Solutions"
+          subtitle="Product Owner"
+        />
+        <TeamCard
+          photo="/rsc/images/team/rodri.png"
+          name="Rodrigo Leal"
+          title="Retail Principal, Industry Solutions"
+          subtitle="Product Owner"
+        />
+        <TeamCard
+          photo="/rsc/images/team/angie.jpeg"
+          name="Angie Güemes"
+          title="Sr. Specialist, Industry Solutions"
+          subtitle="Full Stack Developer"
+        />
+        <TeamCard
+          photo="/rsc/images/team/flor.png"
+          name="Flor Arín"
+          title="Sr. Specialist, Industry Solutions"
+          subtitle="Full Stack Developer"
+        />
+        <TeamCard
+          photo="/rsc/images/team/rakshit.png"
+          name="Rakshit Joshi"
+          title="Operations Specialist, Industry Solutions"
+          subtitle="Maintainer"
+        />
+        {/* Add more TeamMemberCard components as needed */}
+      </div>
+      <hr></hr>
+      <H3>Special thanks</H3>
+      <div className={styles.teamcards}>
+        <TeamCard
+          photo="/rsc/images/team/prashant.png"
+          name="Prashant Juttukonda"
+          title="Retail Principal, Industry Solutions"
+          subtitle="Product Owner"
+        />
+
+        <TeamCard
+          photo="/rsc/images/team/pedro.png"
+          name="Pedro Bereilh"
+          title="Sr. Specialist, Industry Solutions"
+          subtitle="Cloud Engineer"
+        />
+        <TeamCard
+          photo="/rsc/images/team/ainhoa.png"
+          name="Ainhoa Mugica"
+          title="Consultant, Industry Solutions"
+          subtitle="Project Manager"
+        />
+        {/* Add more TeamMemberCard components as needed */}
       </div>
     </Container>
   );
