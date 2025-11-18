02aa4af34ada833d6140c943b699d53b5975b50a
angie <angie.guemes@mongodb.com>
2025-11-18 10:09:15 -0600

    Add AppFeatureWrapper and update FeatureListener

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,33 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './globals.css';
-import { GeistSans } from 'geist/font/sans';
-import ClientProvider from './ClientProvider'; // Adjust the path if needed
-import LoginComp from './_components/login/LoginComp';
-import ChatbotOpener from './_components/chatbot/ChatbotOpener';
-import DigitalReceiptComp from './_components/digitalReceipt/DigitalReceiptComp';
-import AlertsContainer from './_components/alertsContainer/AlertsContainer';
-import FeatureListener from './_components/featureListener/FeatureListener';
+export const dynamic = 'force-dynamic';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./globals.css";
+import { GeistSans } from "geist/font/sans";
+import ClientProvider from "./ClientProvider"; // Adjust the path if needed
+import LoginComp from "./_components/login/LoginComp";
+import ChatbotOpener from "./_components/chatbot/ChatbotOpener";
+import DigitalReceiptComp from "./_components/digitalReceipt/DigitalReceiptComp";
+import AlertsContainer from "./_components/alertsContainer/AlertsContainer";
+import AppFeatureWrapper from "./_components/featureListener/AppFeatureWrapper";
 
 export const metadata = {
-  title: 'Home',
-  description: '',
+  title: "Home",
+  description: "",
 };
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en" className={GeistSans.className}>
       <body>
         <ClientProvider>
-          {children}
-          <FeatureListener/>
-          <LoginComp/>
-          <DigitalReceiptComp/>
-          <ChatbotOpener/>
-          <AlertsContainer/>
+          <AppFeatureWrapper>
+            {children}
+            <LoginComp />
+            <DigitalReceiptComp />
+            <ChatbotOpener />
+            <AlertsContainer />
+          </AppFeatureWrapper>
         </ClientProvider>
       </body>
     </html>
-  )
+  );
 }
